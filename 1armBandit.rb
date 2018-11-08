8bbe5f87f5c3d2eb7ed09b5b66e5f35ff60b572e
George <44814283+Shalfio@users.noreply.github.com>
2018-11-08 11:13:12 +0300

    Add files via upload

---
+++ 1armBandit.rb
@@ -0,0 +1,40 @@
+print "How old are you? "
+age = gets.to_i
+
+print "Do you want to play?(Y/N) "
+answer = gets.strip.capitalize
+
+if answer == "Y" && age >= 18
+	puts "Ok, you are allowed "
+	money = 100
+
+	100.times do
+	print "Try is 10, press any button to contunie... "
+	gets
+	print "You have #{money}$ "
+#Condition
+if money != 0
+money = money - 10
+ 
+	x = rand(0..9)
+	y = rand(0..9)
+	z = rand(0..9)
+	puts " #{x}#{y}#{z}"
+	        #Win
+		if x==y && y==z
+		puts "You won 200$ "
+		money = money + 200
+		end
+	        #GameOver
+		if money == 0
+		puts "You are out of money. "
+		exit
+		end 
+		
+
+
+	end
+	end
+              	
+end 
+
