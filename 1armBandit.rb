25e9bcbf7790c23e754b10ed0fc2a2f625b517ba
George <44814283+Shalfio@users.noreply.github.com>
2018-11-08 12:11:26 +0300

    Update 1armBandit.rb

--- 1armBandit.rb
+++ 1armBandit.rb
@@ -12,14 +12,15 @@ if answer == "Y" && age >= 18
 	print "Try is 10, press any button to contunie... "
 	gets
 	print "You have #{money}$ "
+		
 #Condition
 if money != 0
-money = money - 10
+money = money - 5
  
 	x = rand(0..9)
 	y = rand(0..9)
 	z = rand(0..9)
-	puts " #{x}#{y}#{z}"
+	
 	        #Win
 		if x==y && y==z
 		puts "You won 200$ "
@@ -30,7 +31,9 @@ money = money - 10
 		puts "You are out of money. "
 		exit
 		end 
-		
+	
+	puts "Combination: #{x}#{y}#{z}"
+	puts "Bank#{money}." 
 
 
 	end
